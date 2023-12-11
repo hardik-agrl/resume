6066c26098d0677c5f6f85cd44405fbf8714666a
hardik-agrl <hardik2001agarwal@gmail.com>
2023-12-11 23:09:43 +0530

    changes

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -5,27 +5,33 @@ export default function Education() {
     <div className="container ml-2">
       <h2>EDUCATION</h2>
       <ul>
+        <p className="my-3">
         <h3>B.E</h3>
-        <h4>M S Ramaiah Institute Of Technology</h4>, Bangalore,Karnataka
+        <h4>M S Ramaiah Institute Of Technology</h4> Bangalore,Karnataka
         <ul>
-          <li>7.23/10 CGPA</li>
           <li>08/2019 - 08/2023</li>
+          <li>7.23/10 CGPA</li>
           <li>Electronics and Communication</li>
         </ul>
+        </p>
+        <p className="my-3">
         <h3>XII (CBSE)</h3>
-        <h4>Lord Buddha School</h4> , Kota,Rajasthan
+        <h4>Lord Buddha School</h4>  Kota,Rajasthan
         <ul>
           <li>02/2018 - 04/2019</li>
-          <li>70%</li>
+          <li>65%</li>
           <li>PCM</li>
         </ul>
+        </p>
+        <p className="my-3">
         <h3>X (CBSE)</h3>
-        <h4>Delhi Public School</h4>, Raipur,Chattisgarh
+        <h4>Delhi Public School</h4> Raipur,Chattisgarh
         <ul>
           <li>02/2016 - 04/2017 </li>
           <li>90%</li>
           <li>PCM</li>
         </ul>
+        </p>
       </ul>
     </div>
   );
