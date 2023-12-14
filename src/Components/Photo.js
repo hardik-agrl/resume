7c035c77fa97c776765ab9557e9c4efc1076dd39
hardik-agrl <hardik2001agarwal@gmail.com>
2023-12-14 14:09:55 +0530

    photo changes

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import photo from './image/1ms19ec046.jpg'
-import photo from "./image/Screenshot 2023-11-16 205204.png";
+import photo from "./image/pic.png";
 
 export default function Photo() {
   return (
     <div className="text-center">
-    <img src={photo} class="rounded mx-auto d-block " alt="..."></img>
+    <img height={180}  width={200}  src={photo} class="rounded mx-auto d-block   " alt="..."></img>
     </div>
   );
 }
